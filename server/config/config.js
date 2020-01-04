7d89fca3386e8032624a3fd6a457ca7629cfddab
danivillalobosv <davvsan16@gmail.com>
2020-01-03 18:34:39 -0600

    REST server - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,2 +1,10 @@
 // Puerto
 process.env.PORT = process.env.PORT || 3000;
+
+// Entorno
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// Base de datos
+let urlDB = process.env.NODE_ENV === 'dev' ? 'mongodb://localhost:27017/cafe' : 'bd-remota';
+
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,46 +1,28 @@
 require('./config/config');
 
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 const bodyParser = require('body-parser');
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
- 
+app.use(bodyParser.urlencoded({ extended: false }));
+
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.get('/usuario', function (req, res) {
-    res.json('Post Usuario');
-});
+app.use(require('./routes/usuario'));
 
-app.post('/usuario', function (req, res) {
-    let body = req.body;
-
-    if (!body.nombre) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-    } else {
-        res.json({
-            persona: body
-        });
-    }
-});
-
-// La peticion put actualiza
-app.put('/usuario/:id', function (req, res) {
-
-    let id = req.params.id;
-    res.json({
-        id
-    });
-});
-
-// En una peticion delete no se borra realmente, se cambia el estado para que ya no este disponible.
-app.delete('/usuario', function (req, res) {
-    res.json('Delete Usuario');
+mongoose.connect(process.env.URLDB, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+}, 
+(err, res) => {
+    if (err) throw err;
+    console.log('Base de datos ONLINE');
 });
 
 app.listen(process.env.PORT, () => {
