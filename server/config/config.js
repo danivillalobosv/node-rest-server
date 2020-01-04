87a898b8f9fa2e568c7ab0ad21408e3aefbd6ca9
danivillalobosv <davvsan16@gmail.com>
2020-01-03 19:12:38 -0600

    Variable de entorno MONGO_URI

--- server/config/config.js
+++ server/config/config.js
@@ -5,6 +5,6 @@ process.env.PORT = process.env.PORT || 3000;
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 // Base de datos
-let urlDB = process.env.NODE_ENV === 'dev' ? 'mongodb://localhost:27017/cafe' : 'bd-remota';
+let urlDB = process.env.NODE_ENV === 'dev' ? 'mongodb://localhost:27017/cafe' : process.env.MONGO_URI;
 
 process.env.URLDB = urlDB;
